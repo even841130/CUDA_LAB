08726a13a213f91d10abc72caf388344460cb23b
even841130 <even841130@gmail.com>
2017-12-05 00:38:43 +0800

    Add function cpusecond() in listing 2.6

--- BasicPractice/listing2.5.cu
+++ BasicPractice/listing2.5.cu
@@ -4,6 +4,12 @@
 #include <stdio.h>
 #include <sys/time.h>
 
+double cpuSecond()
+{
+	struct timeval tp;
+	gettimeofday(&tp , NULL);
+	return ((double)tp.tv_sec +(double)tp.tv_usec*1.e-6);
+}
 #define CHECK(call)
 {
 	const cudaError_t error = call;
